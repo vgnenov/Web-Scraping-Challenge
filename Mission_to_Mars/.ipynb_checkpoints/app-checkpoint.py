c0383ac980713ccad7ab27d6f50a6910349d35a1
vgnenov <65702831+vgnenov@users.noreply.github.com>
2020-10-09 00:24:44 -0500

    updates

--- Mission_to_Mars/.ipynb_checkpoints/app-checkpoint.py
+++ Mission_to_Mars/.ipynb_checkpoints/app-checkpoint.py
@@ -1,119 +1,36 @@
-{
- "cells": [
-  {
-   "cell_type": "code",
-   "execution_count": 1,
-   "metadata": {},
-   "outputs": [],
-   "source": [
-    "# #import necessary libraries\n",
-    "# from flask import Flask, render_template\n",
-    "# from flask_pymongo import PyMongo\n",
-    "# import pandas as pd \n",
-    "\n",
-    "\n",
-    "# # from flask import Flask, render_template, redirect\n",
-    "# # from flask_pymongo import PyMongo\n",
-    "# # import scrape_mars"
-   ]
-  },
-  {
-   "cell_type": "code",
-   "execution_count": 7,
-   "metadata": {},
-   "outputs": [
-    {
-     "ename": "NameError",
-     "evalue": "name 'null' is not defined",
-     "output_type": "error",
-     "traceback": [
-      "\u001b[1;31m---------------------------------------------------------------------------\u001b[0m",
-      "\u001b[1;31mNameError\u001b[0m                                 Traceback (most recent call last)",
-      "\u001b[1;32m<ipython-input-7-490fe48d6889>\u001b[0m in \u001b[0;36m<module>\u001b[1;34m\u001b[0m\n\u001b[0;32m      1\u001b[0m \u001b[1;31m# import necessary libraries\u001b[0m\u001b[1;33m\u001b[0m\u001b[1;33m\u001b[0m\u001b[1;33m\u001b[0m\u001b[0m\n\u001b[0;32m      2\u001b[0m \u001b[1;32mfrom\u001b[0m \u001b[0mflask\u001b[0m \u001b[1;32mimport\u001b[0m \u001b[0mFlask\u001b[0m\u001b[1;33m,\u001b[0m \u001b[0mrender_template\u001b[0m\u001b[1;33m\u001b[0m\u001b[1;33m\u001b[0m\u001b[0m\n\u001b[1;32m----> 3\u001b[1;33m \u001b[1;32mimport\u001b[0m \u001b[0mscrape_mars\u001b[0m\u001b[1;33m\u001b[0m\u001b[1;33m\u001b[0m\u001b[0m\n\u001b[0m\u001b[0;32m      4\u001b[0m \u001b[1;32mimport\u001b[0m \u001b[0mpymongo\u001b[0m\u001b[1;33m\u001b[0m\u001b[1;33m\u001b[0m\u001b[0m\n\u001b[0;32m      5\u001b[0m \u001b[1;33m\u001b[0m\u001b[0m\n",
-      "\u001b[1;32m~\\class\\Homework\\Web-Scraping-Challenge\\Mission_to_Mars\\scrape_mars.py\u001b[0m in \u001b[0;36m<module>\u001b[1;34m\u001b[0m\n\u001b[0;32m    148\u001b[0m   {\n\u001b[0;32m    149\u001b[0m    \u001b[1;34m\"cell_type\"\u001b[0m\u001b[1;33m:\u001b[0m \u001b[1;34m\"code\"\u001b[0m\u001b[1;33m,\u001b[0m\u001b[1;33m\u001b[0m\u001b[1;33m\u001b[0m\u001b[0m\n\u001b[1;32m--> 150\u001b[1;33m    \u001b[1;34m\"execution_count\"\u001b[0m\u001b[1;33m:\u001b[0m \u001b[0mnull\u001b[0m\u001b[1;33m,\u001b[0m\u001b[1;33m\u001b[0m\u001b[1;33m\u001b[0m\u001b[0m\n\u001b[0m\u001b[0;32m    151\u001b[0m    \u001b[1;34m\"metadata\"\u001b[0m\u001b[1;33m:\u001b[0m \u001b[1;33m{\u001b[0m\u001b[1;33m}\u001b[0m\u001b[1;33m,\u001b[0m\u001b[1;33m\u001b[0m\u001b[1;33m\u001b[0m\u001b[0m\n\u001b[0;32m    152\u001b[0m    \u001b[1;34m\"outputs\"\u001b[0m\u001b[1;33m:\u001b[0m \u001b[1;33m[\u001b[0m\u001b[1;33m]\u001b[0m\u001b[1;33m,\u001b[0m\u001b[1;33m\u001b[0m\u001b[1;33m\u001b[0m\u001b[0m\n",
-      "\u001b[1;31mNameError\u001b[0m: name 'null' is not defined"
-     ]
-    }
-   ],
-   "source": [
-    "# import necessary libraries\n",
-    "from flask import Flask, render_template\n",
-    "import scrape_mars\n",
-    "import pymongo\n",
-    "\n",
-    "# create instance of Flask app\n",
-    "app = Flask(__name__)\n",
-    "\n",
-    "\n",
-    "# create route that renders index.html template\n",
-    "@app.route(\"/\")\n",
-    "def index():\n",
-    "    #connect to MongoDB\n",
-    "    conn = 'mongodb://localhost:27017'\n",
-    "    client = pymongo.MongoClient(conn)\n",
-    "    db = client.mars_db\n",
-    "    mars_dict = db.mars_data.find_one()\n",
-    "\n",
-    "    client.close()\n",
-    "    return render_template(\"index.html\", dict = mars_dict)\n",
-    "\n",
-    "@app.route(\"/scrape\")\n",
-    "def Scrape():\n",
-    "    #scrape the data\n",
-    "    mars_dict = scrape.scrape()\n",
-    "\n",
-    "    #connect to MongoDB\n",
-    "    conn = 'mongodb://localhost:27017'\n",
-    "    client = pymongo.MongoClient(conn)\n",
-    "\n",
-    "    db = client.mars_db\n",
-    "    db.mars_data.insert_one(mars_dict)\n",
-    "\n",
-    "    client.close()\n",
-    "\n",
-    "    return render_template(\"index.html\", dict = mars_dict)\n",
-    "\n",
-    "\n",
-    "if __name__ == \"__main__\":\n",
-    "    app.run(debug=True)"
-   ]
-  },
-  {
-   "cell_type": "code",
-   "execution_count": null,
-   "metadata": {},
-   "outputs": [],
-   "source": [
-    "#pip install flask_pymongo"
-   ]
-  },
-  {
-   "cell_type": "code",
-   "execution_count": null,
-   "metadata": {},
-   "outputs": [],
-   "source": []
-  }
- ],
- "metadata": {
-  "kernelspec": {
-   "display_name": "Python 3",
-   "language": "python",
-   "name": "python3"
-  },
-  "language_info": {
-   "codemirror_mode": {
-    "name": "ipython",
-    "version": 3
-   },
-   "file_extension": ".py",
-   "mimetype": "text/x-python",
-   "name": "python",
-   "nbconvert_exporter": "python",
-   "pygments_lexer": "ipython3",
-   "version": "3.7.6"
-  }
- },
- "nbformat": 4,
- "nbformat_minor": 4
-}
+# import necessary libraries
+from flask import Flask, render_template
+from flask_pymongo import PyMongo
+import pandas as pd 
+import scrape_mars
+
+# create instance of Flask app
+app = Flask(__name__)
+
+# Use PyMongo to establish Mongo connection
+mongo = PyMongo(app, uri="mongodb://localhost:27017/app.py")
+
+
+# create route that renders index.html template
+@app.route("/")
+def echo():
+     # Find data
+    m_facts = mongo.db.collection.find_one()
+    #Return templated data
+    return render_template("index.html", mars=m_facts)
+
+@app.route("/scrape")
+def scrape():
+    # Run the scrape function
+    mars_data = scrape_mars.scrape_all()
+
+    # Update the Mongo database using update and upsert=True
+    mongo.db.collection.update({}, mars_data, upsert=True)
+
+    # Redirect back to home page
+    return redirect("/", code=302)
+
+
+
+# if __name__ == "__main__":
+#     app.run(debug=True)
--- Mission_to_Mars/app.py
+++ Mission_to_Mars/app.py
@@ -1,124 +1,36 @@
-{
- "cells": [
-  {
-   "cell_type": "code",
-   "execution_count": 1,
-   "metadata": {},
-   "outputs": [],
-   "source": [
-    "# import dependencies\n",
-    "from flask import Flask, render_template, redirect\n",
-    "from flask_pymongo import PyMongo"
-   ]
-  },
-  {
-   "cell_type": "code",
-   "execution_count": 2,
-   "metadata": {},
-   "outputs": [
-    {
-     "name": "stdout",
-     "output_type": "stream",
-     "text": [
-      " * Serving Flask app \"__main__\" (lazy loading)\n",
-      " * Environment: production\n",
-      "   WARNING: This is a development server. Do not use it in a production deployment.\n",
-      "   Use a production WSGI server instead.\n",
-      " * Debug mode: on\n"
-     ]
-    },
-    {
-     "name": "stderr",
-     "output_type": "stream",
-     "text": [
-      " * Restarting with windowsapi reloader\n"
-     ]
-    },
-    {
-     "ename": "SystemExit",
-     "evalue": "1",
-     "output_type": "error",
-     "traceback": [
-      "An exception has occurred, use %tb to see the full traceback.\n",
-      "\u001b[1;31mSystemExit\u001b[0m\u001b[1;31m:\u001b[0m 1\n"
-     ]
-    },
-    {
-     "name": "stderr",
-     "output_type": "stream",
-     "text": [
-      "C:\\Users\\Boston\\anaconda3\\lib\\site-packages\\IPython\\core\\interactiveshell.py:3339: UserWarning: To exit: use 'exit', 'quit', or Ctrl-D.\n",
-      "  warn(\"To exit: use 'exit', 'quit', or Ctrl-D.\", stacklevel=1)\n"
-     ]
-    }
-   ],
-   "source": [
-    "# Create an instance of Flask\n",
-    "app = Flask(__name__)\n",
-    "\n",
-    "# Use PyMongo to establish Mongo connection\n",
-    "mongo = PyMongo(app, uri=\"mongodb://localhost:27017/mars\")\n",
-    "\n",
-    "# create home route and define home function\n",
-    "@app.route(\"/\")\n",
-    "def home():\n",
-    "    # Find one record of data from the mongo database\n",
-    "    mars_info = mongo.db.mars_collection.find_one()\n",
-    "\n",
-    "    # Return template and data\n",
-    "    return render_template(\"index.html\", mars_data=mars_info)\n",
-    "\n",
-    "# create scrape route \n",
-    "@app.route(\"/scrape\")\n",
-    "def scrape():\n",
-    "    # run the scrape function\n",
-    "    mars_data = scrape_mars.scrape()\n",
-    "\n",
-    "    # insert the mars data in to the collection\n",
-    "    mongo.db.mars_collection.update({}, mars_data, upsert=True)\n",
-    "\n",
-    "    # go back to the home page\n",
-    "    return redirect(\"/\")\n",
-    "\n",
-    "# run the app\n",
-    "if __name__ == \"__main__\":\n",
-    "    app.run(debug=True)"
-   ]
-  },
-  {
-   "cell_type": "code",
-   "execution_count": null,
-   "metadata": {},
-   "outputs": [],
-   "source": []
-  },
-  {
-   "cell_type": "code",
-   "execution_count": null,
-   "metadata": {},
-   "outputs": [],
-   "source": []
-  }
- ],
- "metadata": {
-  "kernelspec": {
-   "display_name": "Python 3",
-   "language": "python",
-   "name": "python3"
-  },
-  "language_info": {
-   "codemirror_mode": {
-    "name": "ipython",
-    "version": 3
-   },
-   "file_extension": ".py",
-   "mimetype": "text/x-python",
-   "name": "python",
-   "nbconvert_exporter": "python",
-   "pygments_lexer": "ipython3",
-   "version": "3.7.6"
-  }
- },
- "nbformat": 4,
- "nbformat_minor": 4
-}
+# import necessary libraries
+from flask import Flask, render_template
+from flask_pymongo import PyMongo
+import pandas as pd 
+import scrape_mars
+
+# create instance of Flask app
+app = Flask(__name__)
+
+# Use PyMongo to establish Mongo connection
+mongo = PyMongo(app, uri="mongodb://localhost:27017/app.py")
+
+
+# create route that renders index.html template
+@app.route("/")
+def echo():
+     # Find data
+    m_facts = mongo.db.collection.find_one()
+    #Return templated data
+    return render_template("index.html", mars=m_facts)
+
+@app.route("/scrape")
+def scrape():
+    # Run the scrape function
+    mars_data = scrape_mars.scrape_all()
+
+    # Update the Mongo database using update and upsert=True
+    mongo.db.collection.update({}, mars_data, upsert=True)
+
+    # Redirect back to home page
+    return redirect("/", code=302)
+
+
+
+# if __name__ == "__main__":
+#     app.run(debug=True)
